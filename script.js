82796044823c8f1814dd0a6473a1ee4656467c52
thao15059 <thao15059@gmail.com>
2021-07-03 21:39:11 -0700

    implements navbar

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 document.querySelectorAll(".navigation-button").forEach((item) => {
   item.onclick = () => {
-    console.log(item)
-    item.parentElement.parentElement.classList.toggle('change')
-  }
-})
+    item.parentElement.parentElement.classList.toggle("change");
+  };
+});
+
+document.querySelectorAll(".nav-link").forEach((item) => {
+  item.style.cssText = `background-color: ${item.getAttribute("data-color")}`;
+});
+
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+  document.body.classList.add("stop-scrolling");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+  document.body.classList.remove("stop-scrolling");
+});
